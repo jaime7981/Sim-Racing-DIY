c9ea86194c498ac47ea5cb34e1e9590a99af7bb8
jaime7981 <jmfernandez1@miuandes.cl>
2024-04-03 21:29:49 -0300

    feat: first sequential shifter solution

--- sequential_shifter/src/main.cpp
+++ sequential_shifter/src/main.cpp
@@ -1,18 +1,67 @@
 #include <Arduino.h>
+#include "Joystick.h"
 
-// put function declarations here:
-int myFunction(int, int);
+// Define struct for a switch
+struct Switch {
+  int pin;
+  bool state;
+  int buttonIndex; // Index of the corresponding joystick button
+};
+
+// Define switches
+Switch upShiftButton = {2, false, 0}; // Example pin and button index for upshift button
+Switch downShiftButton = {3, false, 1}; // Example pin and button index for downshift button
+
+// Define joystick object
+Joystick_ Joystick(
+  JOYSTICK_DEFAULT_REPORT_ID,
+  JOYSTICK_TYPE_JOYSTICK,
+  2, // Number of buttons
+  0, // Number of hat switches
+  false, // Include X axis
+  false, // Include Y axis
+  false, // Include Z axis
+  false, // Include Rx axis
+  false, // Include Ry axis
+  false, // Include Rz axis
+  false, // Include Rudder axis
+  false, // Include Throttle axis
+  false // Include Accelerator axis
+);
+
+// Function to read switch state and set joystick button state
+void updateSwitchState(Switch &button) {
+  bool currentState = digitalRead(button.pin);
+
+  if (currentState != button.state) {
+    button.state = currentState;
+
+    if (button.state) {
+      Joystick.releaseButton(button.buttonIndex);
+    } else {
+      Joystick.pressButton(button.buttonIndex);
+    }
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Initialize serial communication
+  Serial.begin(9600);
+
+  // Set button pins as inputs with internal pull-up resistors
+  pinMode(upShiftButton.pin, INPUT_PULLUP);
+  pinMode(downShiftButton.pin, INPUT_PULLUP);
+
+  // Set initial button state
+  updateSwitchState(upShiftButton);
+  updateSwitchState(downShiftButton);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Read and set button states
+  updateSwitchState(upShiftButton);
+  updateSwitchState(downShiftButton);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Small delay to prevent bouncing
+  delay(50);
 }
